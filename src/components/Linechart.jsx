43391a188fffa8c408c19dd4070d7b74818ccea0
daniel-mekuria <mekuriadaniel4@gmail.com>
2024-04-18 13:57:00 -0700

    bar chart and line chart components

---
+++ src/components/Linechart.jsx
@@ -0,0 +1,19 @@
+import { LineChart } from '@tremor/react';
+
+const dataFormatter = (number) =>
+  `$${Intl.NumberFormat('us').format(number).toString()}`;
+
+export default function LineChartHero({chartData}) {
+  return (
+    <LineChart
+      className="h-50 shrink-1"
+      data={chartData}
+      index="date"
+      categories={['SemiAnalysis', 'The Pragmatic Engineer']}
+      colors={['indigo', 'rose']}
+      valueFormatter={dataFormatter}
+      yAxisWidth={120}
+      onValueChange={(v) => console.log(v)}
+    />
+  );
+}
